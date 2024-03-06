27e7d2bf4029630f4cfb9d74b8c631152aacdfbd
clone <luisgustavoplathdisaro@gmail.com>
2024-03-06 11:54:05 -0300

    terceiro dia

--- clone/MauiProgram.cs
+++ clone/MauiProgram.cs
@@ -13,6 +13,8 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				    fonts.AddFont("GalleroVintage-DemoVersion-Regular.otf", "GalleroVintage-DemoVersion-Regular"); 
+
 			});
 
 #if DEBUG
